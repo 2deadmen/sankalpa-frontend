361d1dae611cc16cec4fc2f95aae72e4c157d39e
2deadmen <kartikhegde.2002@gmail.com>
2023-06-04 00:03:40 +0530

    admin page and search bar

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -58,11 +58,13 @@ router.post(
         var result=[]
         snapshot.forEach((doc) => {
             var arr = []
+            arr.push(doc.id)
             arr.push(doc.data().college_name);
             arr.push(doc.data().UTR);
             arr.push(doc.data().ss_link);
             arr.push(doc.data().staff_name);
             arr.push(doc.data().staff_phonenumber);
+            arr.push(doc.data().teamname)
             result.push(arr)
             c++
 
@@ -92,7 +94,7 @@ router.post(
         .get();
         console.log(snapshot._docs())
         if (!snapshot) {
-          return res.status(400).json({ error: "Please try to login again " });
+          return res.status(400).json({ error: "Team id is invalid..please try again " });
         }
        
         
@@ -100,12 +102,56 @@ router.post(
         snapshot.forEach(doc => {
             var arr=[]
             console.log( doc.data().treasurehunt);
+           
+            arr.push(doc.data().treasurehunt)
+            arr.push(doc.data().advertising)
+            arr.push(doc.data().cultural)
+            arr.push(doc.data().photography)
+            arr.push(doc.data().quiz)
+            
+            
+            result.push(arr)
+          });
+       
+       
+        
+        res.json({"registered_users":
+    result,"total":snapshot._size});
+      } catch (error) {
+        res.status(500).send({ error: error.message });
+      }
+    }
+  );
+
+  router.post(
+    "/all_team_details",
+    
+    async (req, res) => {
+     
+  
+      try {
+       
+        const snapshot = await db
+          .collection("team_members")
+        .get();
+        console.log(snapshot._docs())
+        if (!snapshot) {
+          return res.status(400).json({ error: "Team id is invalid..please try again " });
+        }
+       
+        
+        var result=[]
+        snapshot.forEach(doc => {
+            var arr=[]
+           
+            arr.push(doc.data().team_id)
             arr.push(doc.data().treasurehunt)
             arr.push(doc.data().advertising)
             arr.push(doc.data().cultural)
             arr.push(doc.data().photography)
             arr.push(doc.data().quiz
             )
+            
             result.push(arr)
           });
        
--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,9 +1,175 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+
 
 const Admin = () => {
-  return (
-    <div>Admin</div>
-  )
-}
+   
+  const [totalvisitors, settotalvisitors] = useState(0);
+  const [allteams, setallteams] = useState([]);
+  const [teamdetails, setteamdetails] = useState([]);
+  const [allteamdetails, setallteamdetails] = useState([]);
+  const [warning, setwarning] = useState("");
+  const [searchres, setsearchres] = useState([]);
+  useEffect(() => {
+    fetchallteams();
+  }, []);
 
-export default Admin
+  const fetchallteams = async () => {
+    const response = await fetch(
+      "http://localhost:5000/api/admin/registered_teams",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const json = await response.json();
+    if (response.status === 200) {
+      setallteams(json["registered_users"]);
+    }
+  };
+
+  const fetchteamdetails = async (team_id) => {
+    const response = await fetch(
+      "http://localhost:5000/api/admin/team_details",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          team_id: team_id,
+        }),
+      }
+    );
+    const json = await response.json();
+    if (response.status === 200) {
+      setteamdetails(json["registered_users"]);
+    } else {
+      setwarning(json["error"]);
+    }
+  };
+
+  const fetchallteamdetails = async () => {
+    const response = await fetch(
+      "http://localhost:5000/api/admin/all_team_details",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const json = await response.json();
+    if (response.status === 200) {
+      setteamdetails(json["registered_users"]);
+    } else {
+      setwarning(json["error"]);
+    }
+  };
+  const handlesearch = () => {
+    
+    let keys = document.getElementById("searchbar").value.toLowerCase();
+    if (keys === "") {
+      setsearchres([]);
+      return
+    }
+    for (let i = 0; i < allteams.length; i++) {
+        let ele=allteams[i][1].toLowerCase()
+        if (ele.includes(keys.toLowerCase())) {
+         if(!searchres.includes(allteams[i])){
+          setsearchres([...searchres, allteams[i]]);
+         }
+      }
+    }
+    
+  };
+ const handleview=async(team_id)=>{
+ 
+    await fetchteamdetails(team_id)
+  
+
+ }
+ 
+  return (
+    <>
+      {" "}
+      <div>
+        <h1>Admin Panel</h1>
+      </div>
+
+   
+
+      <form class="d-flex" role="search">
+        <input
+          class="form-control me-2"
+          type="search"
+          id="searchbar"
+          onKeyUp={handlesearch}
+          placeholder="Search teams by college name"
+          aria-label="Search"
+        />
+      </form>
+      <table cellPadding={5} cellSpacing={5} border={2}>
+        {searchres !== [] &&
+          searchres.map((ele) => {
+            return (
+              <tr>
+                <td><button className="btn btn-primary" onClick={()=>handleview(ele[0])}>view details</button></td>
+                <td>{ele[6]}</td>
+                <td> {ele[1]}</td>
+                <td> {ele[2]}</td>
+                <td>
+                  {" "}
+                  <a href={ele[3]}>{ele[3]}</a>
+                </td>
+                <td> {ele[4]}</td>
+                <td>
+                  {" "}
+                  <a href={"tel:" + ele[5]}>{ele[5]}</a>
+                </td>
+              </tr>
+            );
+          })}{" "}
+      </table>
+      <h2 align="center">Registered teams</h2>
+      total teams : {allteams.length}
+      <table cellPadding={5} cellSpacing={5} border={2}>
+        <tr>
+
+          <th>team id</th>
+          <th>Team name</th>
+          <th>college name</th>
+          <th>UTR</th>
+          <th>Screenshot</th>
+          <th>staff</th>
+          <th>Staff number </th>
+          <hr />
+        </tr>
+        {allteams !== [] &&
+          allteams.map((ele) => {
+            return (
+              <tr>
+                
+                <td><button className="btn btn-primary" onClick={handleview}>view details</button></td>
+                <td>{ele[6]}</td>
+                <td> {ele[1]}</td>
+                <td> {ele[2]}</td>
+                <td>
+                  {" "}
+                  <a href={ele[3]}>{ele[3]}</a>
+                </td>
+                <td> {ele[4]}</td>
+                <td>
+                  {" "}
+                  <a href={"tel:" + ele[5]}>{ele[5]}</a>
+                </td>
+              </tr>
+            );
+          })}
+      </table>
+    </>
+  );
+};
+
+export default Admin;
