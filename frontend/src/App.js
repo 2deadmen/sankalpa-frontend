211fdf5b65f3f3f877c9817bca16ce743f2ea343
2deadmen <kartikhegde.2002@gmail.com>
2023-06-05 15:19:59 +0530

    first

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React,{Suspense,useState} from "react";
 import Loader from "./Loader";
-import Register from "./pages/Register";
+// import Register from "./pages/Register";
 import Home from "./pages/Home";
-import Regclg from "./pages/Regclg";
-import Buildteam from "./pages/Buildteam";
-import Admin from "./pages/Admin";
-import Admin_login from "./pages/Admin_login";
+
 
 function App() {
   const [loader, setloader] = useState(false);
@@ -16,12 +13,12 @@ function App() {
       <Suspense>
       <Loader  loader={loader}/>
       <Routes>
-        <Route path ='/' element={<Register setloader={setloader}/>}/>
-        {/* <Route path ='/' element={<Home setloader={setloader}/>}/> */}
-        <Route path ='/admin' element={<Admin setloader={setloader}/>}/>
+        {/* <Route path ='/' element={<Register setloader={setloader}/>}/> */}
+        <Route path ='/' element={<Home setloader={setloader}/>}/>
+        {/* <Route path ='/admin' element={<Admin setloader={setloader}/>}/>
         <Route path ='/admin-login' element={<Admin_login setloader={setloader}/>}/>
         <Route path ='/college_registration' element={<Regclg setloader={setloader}/>}/>
-        <Route path ='/build_team' element={<Buildteam setloader={setloader}/>}/>
+        <Route path ='/build_team' element={<Buildteam setloader={setloader}/>}/> */}
       </Routes>
       </Suspense>
     </Router>
